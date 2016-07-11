e56a4d34a1d82e72b122d9bf16f080b6407ae8e3
Sebastian Janzen <sebastian.janzen@innoq.com>
2016-07-11 08:51:20 +0200

    Some changes
     - Ensure resolved promise in before() call
     - Add test if appFolder exists
     - Add test that package.json contains bundleName
     - Add debug code: If java file not found, what does the folder contain?

--- test/app.spec.js
+++ test/app.spec.js
@@ -11,37 +11,59 @@ describe('generator-eclipse-smarthome-ui:app', function () {
     [helpers.createDummyGenerator(), 'eclipse-smarthome-ui:pom']
   ];
 
-  before(function () {
+  const appFolder = 'test.myapp';
+
+  before(function (done) {
     return helpers.run(path.join(__dirname, '../generators/app'))
       .withGenerators(pomGenerator)
       .inTmpDir(function (dir) {
         fs.copySync(path.join(__dirname, 'parent-pom.xml'), path.join(dir, 'pom.xml'));
       })
-      .withPrompts({uiName: 'My New UI', bundleName: 'test.myapp', uiPath: '/test'})
-      .toPromise();
+      .withPrompts({uiName: 'My New UI', bundleName: appFolder, uiPath: '/test'})
+      .toPromise()
+      .then(function (dir) {
+        console.log('Generator temp directory: ', dir);
+        done();
+      }, done);
   });
 
   describe('creates', function () {
 
+    it('target app folder', function () {
+      assert.file(appFolder);
+    });
+
     it('dot files', function () {
       assert.file([
-        'test.myapp/.classpath',
-        'test.myapp/.project',
-        'test.myapp/.gitignore',
-        'test.myapp/.editorconfig'
+        appFolder + '/.classpath',
+        appFolder + '/.project',
+        appFolder + '/.gitignore',
+        appFolder + '/.editorconfig'
       ]);
     });
 
     it('OSGi files', function () {
       assert.file([
-        'test.myapp/OSGI-INF/myapp.xml',
-        'test.myapp/META-INF/MANIFEST.MF'
+        appFolder + '/OSGI-INF/myapp.xml',
+        appFolder + '/META-INF/MANIFEST.MF'
       ]);
-      assert.fileContent('test.myapp/.project', /<name>test\.myapp</);
+      assert.fileContent(appFolder + '/.project', /<name>test\.myapp</);
+    });
+
+    it('has a package.json', function () {
+      assert.jsonFileContent(appFolder + '/package.json', {name: 'test.myapp'});
     });
 
     it('a java file', function () {
-      assert.file('test.myapp/src/main/java/test/myapp/internal/MynewUIApp.java');
+      let servletPath = appFolder + '/src/main/java/test/myapp/internal/';
+      let files = fs.readdirSync(servletPath);
+      try {
+        assert.file(servletPath + '/MyNewUIApp.java');
+      } catch (err) {
+        console.log('Files found: ' + files, err);
+        throw err;
+      }
+      assert.fileContent(servletPath + '/MyNewUIApp.java', 'package test.myapp.internal;');
     });
 
 
